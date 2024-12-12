6038c2d4ee65fccf764f6f4f1a8fc54988348803
TsubasaWoKudasai <jlkramek@gmail.com>
2024-12-12 14:40:27 -0300

    new - PostPostPost

--- PostPostPost/Program.cs
+++ PostPostPost/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PostPostPost.Entities;
+namespace PostPostPost;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Comment c1 = new Comment("Have a nice trip!");
+        Comment c2 = new Comment("Wow ! That's awesome!");
+        Post p1 = new Post(
+            DateTime.Parse("21/06/2018 13:05:44"),
+            "Traveling to NZ",
+            "I'm going to visit this wonderful country!",
+            12);
+        p1.AddComment(c1);
+        p1.AddComment(c2);
+
+        Comment c3 = new Comment("Good night !");
+        Comment c4 = new Comment("May the 4th be with u!");
+        Post p2 = new Post(DateTime.Parse("28/07/2018 23:14:19"),
+            "Good night guys!",
+            "See you tomorrow!",
+            5);
+        p2.AddComment(c3);
+        p2.AddComment(c4);
+
+        Console.WriteLine(p1);
+        Console.WriteLine(p2);
+    }
+}
