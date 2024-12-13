25dab94da0d27b2d5ed44d3364ae41a4deb78d05
TsubasaWoKudasai <jlkramek@gmail.com>
2024-12-13 15:44:54 -0300

    new - LeituraArquivo

---
+++ LeituraArquivo/Entities/Item.cs
@@ -0,0 +1,25 @@
+﻿namespace LeituraArquivo.Entities;
+
+class Item
+{
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public int Quantity { get; set; }
+
+    Item()
+    {
+    }
+
+    public Item(string name, double price, int quantity)
+    {
+        Name = name;
+        Price = price;
+        Quantity = quantity;
+    }
+
+    public double Total()
+    {
+        double total = Quantity * Price;
+        return total;
+    }
+}
---
+++ LeituraArquivo/Program.cs
@@ -0,0 +1,47 @@
+﻿using LeituraArquivo.Entities;
+
+
+
+namespace LeituraArquivo;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter file full path: ");
+            string sourceFilePath = Console.ReadLine();
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourceFilePath);
+
+                string sourceFolderPath = Path.GetDirectoryName(sourceFilePath);
+                string targetFolderPath = sourceFolderPath + @"\out";
+                string targetFilePath = targetFolderPath + @"\arquivoTeste2.txt";
+
+                Directory.CreateDirectory(targetFolderPath);
+
+                using (StreamWriter sw = File.AppendText(targetFilePath))
+                {
+                    foreach (string line in lines)
+                    {
+
+                        string[] fields = line.Split(',');
+                        string name = fields[0];
+                        double price = double.Parse(fields[1]);
+                        int quantity = int.Parse(fields[2]);
+
+                        Item prod = new Item (name, price, quantity);
+
+                        sw.WriteLine(prod.Name + "," + prod.Total().ToString("F2"));
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
