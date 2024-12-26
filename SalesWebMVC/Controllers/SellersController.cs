2355a4e831d5425d0a2525b8eb7811e012694f30
TsubasaWoKudasai <jlkramek@gmail.com>
2024-12-26 11:24:40 -0300

    new - Exception handling for delete

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using SalesWebMVC.Models.ViewModels;
 using SalesWebMVC.Models;
 using SalesWebMVC.Services;
 using System.Diagnostics;
+using SalesWebMVC.Services.Exceptions;
 
 
 namespace SalesWebMVC.Controllers
@@ -63,8 +64,15 @@ namespace SalesWebMVC.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Delete(int id)
         {
-            await _sellerService.RemoveAsync(id);
-            return RedirectToAction(nameof(Index));
+            try
+            {
+                await _sellerService.RemoveAsync(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (IntegrityException e)
+            {
+                return RedirectToAction(nameof(Error), new { message = e.Message });
+            }
         }
 
         public async Task<IActionResult> Details(int? id)
